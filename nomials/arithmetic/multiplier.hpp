3fe8f58db0e716b402790ec8de3f74c5e6e07607
arkrp <nexec64@gmail.com>
2018-04-02 21:39:47 -0700

    added multiplication

---
+++ nomials/arithmetic/multiplier.hpp
@@ -0,0 +1,20 @@
+#ifndef MULTIPLY
+#define MULTIPLY
+#include "nomial.hpp"
+template<class T>
+class multiplier : public nomial<T>
+{
+public:
+	nomial<T> * nom1;
+	nomial<T> * nom2;
+	multiplier(nomial<T> * nom1a,nomial<T> * nom2a)
+	{
+		nom1=nom1a;
+		nom2=nom2a;
+	}
+	virtual T eval(T thing)
+	{
+		return(nom1->eval(thing)*nom2->eval(thing));
+	}
+};
+#endif
