b5f830c0cb49b79eedc613e7241eba98c2385efe
arkrp <nexec64@gmail.com>
2018-04-02 21:49:16 -0700

    actualy added tangent this time

---
+++ nomials/trigenometry/tangent.hpp
@@ -0,0 +1,21 @@
+#ifndef TANGENT
+#define TANGENT
+#include "nomial.hpp"
+#include <math.h>
+template<class T>
+class tangent : public nomial<T>
+{
+public:
+	nomial<T> * nom1;
+	//nomial<T> * nom2;
+	tangent(nomial<T> * nom1a)//,nomial<T> * nom2a)
+	{
+		nom1=nom1a;
+		//nom2=nom2a;
+	}
+	virtual T eval(T thing)
+	{
+		return(tan(nom1->eval(thing)));//*nom2->eval(thing));
+	}
+};
+#endif
