1e85a194419e75fc07cbb5126f88e781d1f4975d
MashawnH <37811879+MashawnH@users.noreply.github.com>
2018-04-16 20:10:51 -0700

    Tests
    Testing the original arithmetic classes

--- polynomials.cpp
+++ polynomials.cpp
@@ -11,6 +11,7 @@
 #include "nomials\fundemental\fundemental.hpp"
 #include "nomials\arithmetic\arithmetic.hpp"
 #include "nomials\trigenometry\trigenometry.hpp"
+#include "nomials\arithmetic\divide.hpp"
 using namespace std;
 
 void testnomial(nomial<double> * func);
@@ -22,6 +23,19 @@ int main() {
 	nomial<double> * func1 = new adder<double>(new sine<double>(new identity<double>()),new identity<double>());
 	testnomial(func1);
 
+	//func2 = 3x
+	nomial<double> * func2 = new multiplier<double>(new constant<double>(3),new identity<double>());
+	testnomial(func2);
+
+	//func3 = (3+x)/x
+	nomial<double> * func3 = new divider<double>(new adder<double>(new constant<double>(3), new identity<double>() ) ,new identity<double>());
+	testnomial(func3);
+
+	//func4 = (3+x*sin(x) - tan(x))/(x*cos(3.3x)) = (3 + ((x*sin(x)) - tan(x))) / (x*cos(3.3*x))
+	nomial<double> * func4 = new divider<double>(new suber<double>(new adder<double>(new constant<double>(3), new multiplier<double>(new identity<double>(), new sine<double>(new identity<double>()) )), new tangent<double>(new identity<double>())), /*denom*/ new multiplier<double>(new identity<double>(), new cosine<double>(new multiplier<double>(new constant<double>(3.3), new identity<double>()))));
+	testnomial(func4);
+
+
 
 	return 0;
 }
@@ -31,5 +45,6 @@ void testnomial(nomial<double> * func)
 	cout << "1: " << func->eval(1) << endl;
 	cout << "2: " << func->eval(2) << endl;
 	cout << "3: " << func->eval(3) << endl;
-	cout << "4: " << func->eval(4) << endl;
+	cout << "4: " << func->eval(9) << endl;
+	cout << "----------------" << endl;
 }
